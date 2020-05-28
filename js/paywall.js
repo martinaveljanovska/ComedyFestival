b314fe7bf563868309132b63ea905038a03e38a3
Stef Naskova <snaskova094@gmail.com>
2020-05-28 15:22:29 +0200

    chore: remove all redundant code; added ajax call for asset title and desc.

--- js/paywall.js
+++ js/paywall.js
@@ -1,60 +1,16 @@
 // let packages = ["48884"];
 let service_url = "https://services.inplayer.com";
-
-// create item element
-function createItemElement(id, img, title) {
-	var output = `<div class="package-item"><div class="content" style="background-image:url(${img})"></div><h3 class="item-label">${title}</h3><a href="./item.html?id=${id}" class="overlay-link"></a></div>`;
-	return output;
-}
+var paywall = new InplayerPaywall('036d5d1d-7166-4985-9858-2d1d2c039bc8', [{ id: 102596 }]);
 
 $(function () {
-	$("#preview-item").html(
-		`<div class="inplayer-paywall" id="inplayer-${getParameterByName(
-			"id"
-		)}"></div>`
-	);
 
-	const prependTitleForAsset = (
-		activeAssetId,
-		assetId,
-		assetTitle,
-		assetDesc
-	) => {
-		if (activeAssetId == assetId) {
-			$(`.item-title`).html(assetTitle);
-			$("#item-description").html(assetDesc);
+	$.ajax({
+		url: `${service_url}/items/102596`,
+		success: response => {
+			$('#item-title').html(response.metahash.preview_title);
+			$('#item-description').html(response.metahash.preview_description);
 		}
-	};
-
-	// fetch package
-	const fetchPackage = (packageId) => {
-		$.get(
-			`${service_url}/items/packages/${packageId}/items?limit=100`,
-			(resp) => {
-				let output = "";
-				let activeAssetId = getParameterByName("id");
-
-				for (item of resp.collection) {
-					let assetId = item.id,
-						assetPhoto = item.metahash.paywall_cover_photo,
-						assetTitle = item.metahash.preview_title,
-						assetDesc = item.metahash.preview_description;
-
-					output += createItemElement(assetId, assetPhoto, assetTitle);
-
-					if (activeAssetId) {
-						prependTitleForAsset(activeAssetId, assetId, assetTitle, assetDesc);
-					}
-
-					document.getElementById(
-						`package-items-${packageId}`
-					).innerHTML = output;
-				}
-			}
-		);
-	};
-
-	packages.map((packageId) => fetchPackage(packageId));
+	});
 
 	$(".inplayer-paywall-logout").hide();
 
@@ -67,11 +23,4 @@ $(function () {
 		location.reload();
 	});
 
-	paywall.on("access", (event, asset) => {
-		if (asset.asset.id === 98547 && asset.hasAccess == true) {
-			$("body").show();
-		} else if (asset.asset.id === 98547 && asset.hasAccess == false) {
-			window.location.replace("login.html");
-		}
-	});
 });
