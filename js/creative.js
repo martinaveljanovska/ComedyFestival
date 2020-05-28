e900d4e124fc08d22156935b4641892936d78949
martinaveljanovska <martina.veljanovska@gmail.com>
2020-05-28 16:47:13 +0200

    style changes

--- js/creative.js
+++ js/creative.js
@@ -1,89 +1,88 @@
 function getParameterByName(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return "";
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 $(function () {
-  // ACCORDION 
-  $(".toggle").click(function (e) {
-    e.preventDefault();
-    let inner = $(this).next(".inner");
+	// ACCORDION
+	$(".toggle").click(function (e) {
+		e.preventDefault();
+		let inner = $(this).next(".inner");
 
-    if ($(this).hasClass("active")) {
-      inner.removeClass("show");
-      inner.slideUp(350);
-      $(this).removeClass("active");
-    } else {
-      $(this)
-        .closest(".accordion")
-        .find("a.active")
-        .removeClass("active");
-      $(this)
-        .closest(".accordion")
-        .find(".inner")
-        .not(inner)
-        .removeClass("show")
-        .slideUp(350);
-      inner.slideDown(350);
-      inner.addClass("show");
-      $(this).addClass("active");
-    }
-  });
+		if ($(this).hasClass("active")) {
+			inner.removeClass("show");
+			inner.slideUp(350);
+			$(this).removeClass("active");
+		} else {
+			$(this).closest(".accordion").find("a.active").removeClass("active");
+			$(this)
+				.closest(".accordion")
+				.find(".inner")
+				.not(inner)
+				.removeClass("show")
+				.slideUp(350);
+			inner.slideDown(350);
+			inner.addClass("show");
+			$(this).addClass("active");
+		}
+	});
 
-  // jQuery for page scrolling feature - requires jQuery Easing plugin
-  $('.page-scroll').bind('click', function (event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top - 50)
-    }, 250, 'easeOutSine');
-    event.preventDefault();
-  });
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(".page-scroll").bind("click", function (event) {
+		var $anchor = $(this);
+		$("html, body")
+			.stop()
+			.animate(
+				{
+					scrollTop: $($anchor.attr("href")).offset().top - 50,
+				},
+				250,
+				"easeOutSine"
+			);
+		event.preventDefault();
+	});
 
+	// back to top button
+	$(document).ready(function () {
+		var offset = 550;
+		var duration = 300;
 
-  // back to top button
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > offset) {
+				$(".back-to-top").fadeIn(duration);
+			} else {
+				$(".back-to-top").fadeOut(duration);
+			}
+		});
 
-  var offset = 550;
-  var duration = 300;
+		$(".back-to-top").click(function (event) {
+			event.preventDefault();
 
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > offset) {
-      $('.back-to-top').fadeIn(duration);
-    } else {
-      $('.back-to-top').fadeOut(duration);
-    }
+			$("html, body").animate({ scrollTop: 0 }, duration);
+			return false;
+		});
+	});
 
-  });
+	// mobile menu
+	var toggle = $("#toggle");
 
-  $('.back-to-top').click(function (event) {
-    event.preventDefault();
+	$("#menu a").on("click", function () {
+		console.log();
+		if ($(window).width() < 790) {
+			$("#toggle").click();
+		}
+	});
 
-    $('html, body').animate({ scrollTop: 0 }, duration);
-    return false;
-  })
+	toggle.on("click", function () {
+		$(this).toggleClass("is-active");
 
-
-
-
-  var toggle = $('#toggle');
-
-  $('#menu a').on('click', function () {
-    console.log()
-    if ($(window).width() < 790) {
-      $('#toggle').click();
-    }
-
-  });
-
-  toggle.on('click', function () {
-    $(this).toggleClass('is-active');
-
-    $("#menu").addClass('flex');
-    $('#menu').toggleClass('slideInDown slideOutUp');
-    $('body').toggleClass('overflow');
-  })
-})
+		$("#menu").addClass("flex");
+		$("#menu").toggleClass("slideInDown slideOutUp");
+		$("body").toggleClass("overflow");
+	});
+});
